da1e527893c701e7f434c7757661d61ac39002a3
laizahpimentel <laizahpimentel@gmail.com>
2022-10-28 11:14:06 -0300

    IZA/2810-sync api pokemons

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const fetchPokemon = () => {
-    const getPokemonUrl = `https://pokeapi.com/api/v2/pokemon/${id}`
+    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
     
+    const pokemonPromises = [] 
+
     for ( let i = 1; i <= 150; i++){
-        fetch(url).then(response => response.json())
+        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
+        // fetch(url).then(response => response.json())
     }
+
+    Promise.all(pokemonPromises)
+    .then(pokemons => {
+        //obteve uma array com + 150 pokemons
+        console.log(pokemons)
+
+        const listPokemons = pokemons.reduce((accumulator, pokemon) => {
+            const types = pokemon.types.map(typwInfo => typeInfo.type.name)
+
+            accumulator +=
+            `<li class="card">
+            <img class="card-image ${types[0]}" />
+            <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+            <p class="card-subtitle">${pokemon.types.map(typeInfo => typeInfo.type.name).join('' | '')}</p>
+            </li>
+            `
+            return accumulator
+        }, '')
+        //17:41
+        console.log(listPokemons)
+    })
 }
 
 // Requisiçoes Assincronas - Metodo fetch requisiçao HTPP/URL
